2b38079c5e60a31fd4676da8c0c82ccfd3859a51
Manju373 <144766479+Manju373@users.noreply.github.com>
2026-03-23 13:47:39 +0530

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+print("Welcome to the AI chatbot lesson!")
+name=input("What is your name?")
+print("nice to meet you, " , name , "!")
+mood=input("How are you feeling today?")
+if mood=="bad" :
+    print("i hope your day gets better!")
+elif mood=="good" :
+    print("that's great to hear!")
+else: 
+    print("401 not found")
